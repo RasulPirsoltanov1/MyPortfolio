75d223679ff4514d8c8a04756c29b90295ae9e5c
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-09-16 00:45:34 +0400

    first commit

---
+++ MyPortolio/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <div className='body'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/experience' element={<Experience />} />
+        </Routes>
+        </div>
+        <Footer />
+      </Router>
+
+    </div>
+  )
+}
+
+export default App
---
+++ MyPortolio/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location])
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <img className='logoImg' src='../../public/Rasul_Pirsoltanov.png'/>
+      </div>
+      <div className='toggleButton'>
+        <ReorderIcon onClick={() => {
+          setExpandNavbar(!expandNavbar)
+        }} />
+      </div>
+      <div className='links' id={expandNavbar ? "open" : "close"}>
+        <Link to={"/"} className='link'>Home</Link>
+        <Link to={"/projects"} className='link'>Projects</Link>
+        <Link to={"/experience"} className='link'>Experience</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
